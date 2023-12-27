e35f22ab383f97237c80ef81c497bf250e4fdf45
DmitriyAlexeewich <50995304+DmitriyAlexeewich@users.noreply.github.com>
2023-12-27 16:56:57 +0300

    Add project files.

---
+++ Curs/Domain/HashDictionary.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+namespace Curs.Domain
+{
+    public class HashDictionary<T, Y> where T : notnull
+    {
+        private readonly Hashtable _dictionary;
+
+        public int Length => _dictionary.Count;
+        public T[] Keys => GetItems<T>(_dictionary.Keys);
+        public Y[] Values => GetItems<Y>(_dictionary.Values);
+
+        public HashDictionary()
+        {
+            _dictionary = new Hashtable();
+        }
+
+        public void Add(T key, Y value)
+        {
+            try
+            {
+                if(key is null)
+                    throw new ArgumentNullException(nameof(key));
+
+                _dictionary.Add(key, value);
+            }
+            catch
+            {
+                throw new ArgumentException($"Dictionary already contains key: {key}");
+            }
+        }
+
+        public void Remove(T key)
+        {
+            try
+            {
+                if (key is null)
+                    throw new ArgumentNullException(nameof(key));
+
+                _dictionary.Remove(key);
+            }
+            catch
+            {
+                throw new ArgumentException($"Dictionary not contains key: {key}");
+            }
+        }
+
+        public Y GetElement(T key)
+        {
+            try
+            {
+                if (key is null)
+                    throw new ArgumentNullException(nameof(key));
+
+                var element = _dictionary[key];
+                
+                return (Y)element;
+            }
+            catch
+            {
+                throw new ArgumentException($"Dictionary not contains key: {key}");
+            }
+        }
+
+        private K[] GetItems<K>(ICollection collection)
+        {
+            var elements = new List<K>();
+
+            foreach (var element in collection)
+                elements.Add((K)element);
+
+            return elements.ToArray();
+        }
+    }
+}
---
+++ Curs/Program.cs
@@ -0,0 +1,111 @@
+﻿using Curs.Domain;
+
+var dictionary = new HashDictionary<string, string>();
+
+while (true)
+{
+    Console.WriteLine("1-Add");
+    Console.WriteLine("2-Remove");
+    Console.WriteLine("3-Get all keys");
+    Console.WriteLine("4-Get all items");
+    Console.WriteLine("5-Get item by key");
+    Console.WriteLine("6-Get length");
+    Console.WriteLine("0-Exit");
+
+    Console.WriteLine("Comm: ");
+    var commStr = Console.ReadLine();
+
+    if (!int.TryParse(commStr, out var comm))
+        continue;
+
+    try
+    {
+        switch (comm)
+        {
+            case 1:
+                Add();
+                break;
+            case 2:
+                Remove();
+                break;
+            case 3:
+                ShowArray<string>(dictionary.Keys);
+                break;
+            case 4:
+                ShowArray<string>(dictionary.Values);
+                break;
+            case 5:
+                GetElem();
+                break;
+            case 6:
+                Console.WriteLine(dictionary.Length);
+                break;
+            default:
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    if (comm == 0)
+        return;
+
+    Console.WriteLine();
+    Console.WriteLine("-----------------------");
+    Console.WriteLine();
+}
+
+void Add()
+{
+    Console.WriteLine("Enter key: ");
+    var key = Console.ReadLine();
+
+    if (key is null)
+    {
+        Console.WriteLine("Key is null. Try again");
+        return;
+    }
+
+    Console.WriteLine("Enter value: ");
+    var value = Console.ReadLine();
+
+    dictionary.Add(key, value);
+}
+
+void Remove()
+{
+    Console.WriteLine("Enter key: ");
+    var key = Console.ReadLine();
+
+    if (key is null)
+    {
+        Console.WriteLine("Key is null. Try again");
+        return;
+    }
+
+    dictionary.Remove(key);
+}
+
+void ShowArray<T>(T[] array)
+{
+    foreach(var elem in array)
+        Console.WriteLine($"{elem}");
+}
+
+void GetElem()
+{
+    Console.WriteLine("Enter key: ");
+    var key = Console.ReadLine();
+
+    if (key is null)
+    {
+        Console.WriteLine("Key is null. Try again");
+        return;
+    }
+
+    var elem = dictionary.GetElement(key);
+
+    Console.WriteLine(elem);
+}
